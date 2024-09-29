67ef8422aa50bbbf1121c9b144cc3d0d3b4ef681
Maathusan Sathiendran <155667928+maat2405@users.noreply.github.com>
2024-09-29 09:36:31 -0400

    Updated game page to include button for Tictactoe
    Added new XML styling for rounded buttons

--- app/src/main/java/com/ungubunga/eduventure/fragment_three.kt
+++ app/src/main/java/com/ungubunga/eduventure/fragment_three.kt
@@ -1,5 +1,6 @@
 package com.ungubunga.eduventure
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -20,7 +21,12 @@ class fragment_three : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    /*
+    fun playTicTac(view: View) {
+        val intent = Intent(this, fragment_three ::class.java)
+        startActivity(intent)
+    }
+    */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
