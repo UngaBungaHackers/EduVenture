09e144ffcf13796aaf9747f6cf883c2ae8c50828
Maathusan Sathiendran <155667928+maat2405@users.noreply.github.com>
2024-09-29 11:11:39 -0400

    Tictactoe properly implemented

--- app/src/main/java/com/ungubunga/eduventure/PlayerSetup.kt
+++ app/src/main/java/com/ungubunga/eduventure/PlayerSetup.kt
@@ -3,6 +3,7 @@ package com.ungubunga.eduventure.ui.theme
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -22,6 +23,11 @@ class PlayerSetup : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        var btn = findViewById<Button>(R.id.button2)
+        btn.setOnClickListener{
+            val intent = Intent(this, GameDisplay :: class.java)
+            startActivity(intent)
         }
     }
 
--- app/src/main/java/com/ungubunga/eduventure/TicMain.kt
+++ app/src/main/java/com/ungubunga/eduventure/TicMain.kt
@@ -3,6 +3,7 @@ package com.ungubunga.eduventure.ui.theme
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,10 +14,11 @@ class TicMain : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.tic_main)
-    }
-    fun (view: View) {
-        val intent = Intent(this, PlayerSetup:: class.java)
-        startActivity(intent)
+        var btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener{
+            val intent = Intent(this, PlayerSetup :: class.java)
+            startActivity(intent)
+        }
     }
 }
 
